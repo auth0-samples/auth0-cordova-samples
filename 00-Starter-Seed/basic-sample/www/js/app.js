92653fc3ca8d15204dff223f2be1ff8f5d006631
Sebastian Cancinos <sebacancinos@gmail.com>
2016-08-31 11:38:24 -0300

    Replaced Lock9 with Lock10 and updated code

--- 00-Starter-Seed/basic-sample/www/js/app.js
+++ 00-Starter-Seed/basic-sample/www/js/app.js
@@ -1,34 +1,38 @@
 document.addEventListener('deviceready',function() {
-    var lock = new Auth0Lock(
-      // All these properties are set in auth0-variables.js
-      AUTH0_CLIENT_ID,
-      AUTH0_DOMAIN
-    );
+    var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+      auth: {
+        params: { scope: 'openid email' } //Details: https://auth0.com/docs/scopes
+      }
+    });
 
     var userProfile;
 
     $('.btn-login').click(function(e) {
       e.preventDefault();
-      lock.show(function(err, profile, token) {
-        if (err) {
-          // Error callback
-          console.log("There was an error");
-          alert("There was an error logging in");
-        } else {
-          // Success calback
+      lock.show();
+    });
 
-          // Save the JWT token.
-          localStorage.setItem('userToken', token);
-
-          // Save the profile
-          userProfile = profile;
-
-          $('.login-box').hide();
-          $('.logged-in-box').show();
-          $('.nickname').text(profile.nickname);
-          $('.nickname').text(profile.name);
-          $('.avatar').attr('src', profile.picture);
+    lock.on("authenticated", function(authResult) {
+      lock.getProfile(authResult.idToken, function(error, profile) {
+        if (error) {
+          // Handle error
+            console.log("There was an error");
+            alert("There was an error logging in");
+            return;
         }
+            // Success calback
+
+            // Save the JWT token.
+            localStorage.setItem('userToken', token);
+
+            // Save the profile
+            userProfile = profile;
+
+            $('.login-box').hide();
+            $('.logged-in-box').show();
+            $('.nickname').text(profile.nickname);
+            $('.nickname').text(profile.name);
+            $('.avatar').attr('src', profile.picture);
       });
     });
 
