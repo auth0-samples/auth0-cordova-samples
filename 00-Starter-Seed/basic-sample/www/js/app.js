a3db1f82d87e867f0d94e092d2501a26c77b0090
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-04-06 17:43:27 -0300

    remove calling apis from sample

--- 00-Starter-Seed/basic-sample/www/js/app.js
+++ 00-Starter-Seed/basic-sample/www/js/app.js
@@ -28,30 +28,9 @@ document.addEventListener('deviceready',function() {
           $('.nickname').text(profile.nickname);
           $('.nickname').text(profile.name);
           $('.avatar').attr('src', profile.picture);
+          $('.token').text(token);
         }
       });
     });
-
-    $.ajaxSetup({
-      'beforeSend': function(xhr) {
-        if (localStorage.getItem('userToken')) {
-          xhr.setRequestHeader('Authorization',
-                'Bearer ' + localStorage.getItem('userToken'));
-        }
-      }
-    });
-
-    $('.btn-api').click(function(e) {
-      // Just call your API here. The header will be sent
-      $.ajax({
-        url: 'http://auth0-nodejsapi-sample.herokuapp.com/secured/ping',
-        method: 'GET'
-      }).then(function(data, textStatus, jqXHR) {
-        alert("The request to the secured enpoint was successfull");
-      }, function() {
-        alert("You need to download the server seed and start it to call this API");
-      });
-    });
-
 
 }, false);
--- 00-Starter-Seed/phonegap-basic-sample/README.md
+++ 00-Starter-Seed/phonegap-basic-sample/README.md
@@ -1,6 +1,6 @@
-# Auth0 + phonegap + API Seed
+# Auth0 + phonegap
 
-This is the seed project you need to use if you're going to create an app that will use Auth0, phonegap and an API that you're going to be developing. That API can be in any language.
+This is the seed project you need to use if you're going to create an app that will use Auth0 and phonegap.
 
 ## Running the example
 
@@ -11,4 +11,3 @@ Once you have that, just clone the project and run the following:
 3. `phonegap emulate ios`
 
 Enjoy your phonegap app now :).
-
